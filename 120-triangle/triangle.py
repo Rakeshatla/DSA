a87f921d1e3b42f2babb1a6b050d3af15baac74d
Atla Rakesh Reddy <151203741+Rakeshatla@users.noreply.github.com>
2025-07-09 22:59:19 +0530

    Time: 11 ms (20.52%) | Memory: 19.7 MB (19.15%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,13 +1,14 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n=len(triangle)
+        dp=[[-1 for i in range(j+1)] for j in range(n)]
         def dfs(i,j,memo):
-            if((i,j) in memo):
-                return memo[(i,j)]
+            if(memo[i][j]!=-1):
+                return memo[i][j]
             if(i==n-1):
                 return triangle[n-1][j]
             d=triangle[i][j]+dfs(i+1,j,memo)
             dg=triangle[i][j]+dfs(i+1,j+1,memo)
-            memo[(i,j)]=min(d,dg)
-            return memo[(i,j)]
-        return dfs(0,0,{})
+            memo[i][j]=min(d,dg)
+            return memo[i][j]
+        return dfs(0,0,dp)
