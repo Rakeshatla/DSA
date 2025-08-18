584f07b7fcca9921b3f6f20beb7a40ee3e96a113
Atla Rakesh Reddy <151203741+Rakeshatla@users.noreply.github.com>
2025-08-18 23:17:47 +0530

    Time: 1869 ms (5.01%) | Memory: 30.6 MB (19.46%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,11 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        min_coins = [amount + 1] * (amount + 1)
-        min_coins[0] = 0
-
-        for i in range(1, amount + 1):
-            for c in coins:
-                if i - c >= 0:
-                    min_coins[i] = min(min_coins[i], 1 + min_coins[i - c])
-        
-        return min_coins[-1] if min_coins[-1] != amount + 1 else -1
+        n=len(coins)
+        def dfs(i,target,memo):
+            if(i==0):
+                if(target%coins[0]==0):
+                    return target//coins[0]
+                else:
+                    return float('inf')
+            if((i,target) in memo):
+                return memo[(i,target)]
+            nopick=dfs(i-1,target,memo)
+            pick=float('inf')
+            if(coins[i]<=target):
+                pick=1+dfs(i,target-coins[i],memo)
+            memo[(i,target)]=min(pick,nopick)
+            return min(pick,nopick)
+        a=dfs(n-1,amount,{})
+        return -1 if a==float('inf') else a
