278978a61524e210c133793d64d42211a32d1e82
Atla Rakesh Reddy <151203741+Rakeshatla@users.noreply.github.com>
2025-09-06 23:47:22 +0530

    Time: 2 ms (19.32%) | Memory: 18 MB (69.30%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,18 +1,25 @@
+from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def dfs(row,col,ans,img,delrow,delcol,color,inicolor):
-            ans[row][col]=color
-            n=len(image)
-            m=len(image[0])
+        original=image[sr][sc]
+        m=len(image)
+        n=len(image[0])
+        visited=image.copy()
+        queue=deque([[sr,sc]])
+        visited[sr][sc]=color
+        while queue:
+            node=queue.popleft()
+            print(node)
+            r=node[0]
+            c=node[1]
+            # print(r,c)
+            drow=[-1,0,1,0]
+            dcol=[0,1,0,-1]
             for i in range(4):
-                roww=row+delrow[i]
-                coll=col+delcol[i]
-                if(roww>=0 and roww<n and coll>=0 and coll<m and image[roww][coll]==inicolor and ans[roww][coll]!=color):
-                    dfs(roww,coll,ans,img,delrow,delcol,color,inicolor)
-        ans=image
-        inicolor=image[sr][sc]
-        delrow=[-1,0,1,0]
-        delcol=[0,1,0,-1]
-        dfs(sr,sc,ans,image,delrow,delcol,color,inicolor)
-        return ans
-        
+                nrow=r+drow[i]
+                ncol=c+dcol[i]
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and image[nrow][ncol]==original and visited[nrow][ncol]!=color):
+                    visited[nrow][ncol]=color
+                    queue.append([nrow,ncol])
+        return visited
+
