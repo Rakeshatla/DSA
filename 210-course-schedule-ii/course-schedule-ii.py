b4ff31f6c33c16aaa9124dd10942eb582da9a51d
Atla Rakesh Reddy <151203741+Rakeshatla@users.noreply.github.com>
2025-09-07 23:50:38 +0530

    Time: 4 ms (48.61%) | Memory: 19.1 MB (72.00%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,20 @@
+from collections import deque, defaultdict
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        order = []
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return order if len(order) == numCourses else []
+        
